feda7d42d3dc08028baab5ff1d8c4e061c937b19
Ajju <abhamre50@gmail.com>
2023-03-11 22:19:07 +0530

    till react routers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,20 +24,20 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <a className="nav-link active" aria-current="page" href="/">
                 Home
-              </Link>
+              </a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/about">
+              <a className="nav-link active" aria-current="page" href="/">
                 About
-              </Link>
+              </a>
             </li>
-            {/* <li className="nav-item">
+            <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">
                 Contact
               </a>
-            </li> */}
+            </li>
           </ul>
           {/* <form className="d-flex" role="search">
             <input
